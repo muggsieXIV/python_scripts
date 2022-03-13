18a3a8d6aaf79c84da90468fbf3e86e2d69a084a
muggsieXIV <benwillwalk@gmail.com>
2022-03-13 03:18:41 -0600

    First commit

---
+++ automation-tasks/job_tracker.py
@@ -0,0 +1,44 @@
+import requests
+import json
+from send_email import send_email
+
+URL = "https://remoteok.io/api"
+keys = ['date', 'company', 'position', 'tags', 'location', 'url']
+
+wanted_tags = ["python", "django", "react", "colorado"] # remote, javascript, backend, mobile, ...
+
+def get_jobs():
+    resp = requests.get(URL)
+    job_results = resp.json()
+    
+    jobs = []
+    for job_res in job_results:
+        # take only the specified keys
+        job = {k: v for k, v in job_res.items() if k in keys}
+    
+        if job:
+            tags = job.get('tags')
+            tags = {tag.lower() for tag in tags}
+            if tags.intersection(wanted_tags):
+                jobs.append(job)
+    
+    return jobs
+
+
+if __name__ == '__main__':
+    python_jobs = get_jobs()
+    
+    if python_jobs:
+        message = "Subject: Remote Python Jobs!\n\n"
+        message += "Found some cool Python jobs!\n\n"
+        
+        for job in python_jobs:
+            message += f"{json.dumps(job)}\n\n"
+
+        send_email(message)
+
+
+# Needed to config Certificates.command 
+# Used the following commang
+## open /Applications/Python\ 3.7/Install\ Certificates.command 
+### Explained here https://stackoverflow.com/questions/42098126/mac-osx-python-ssl-sslerror-ssl-certificate-verify-failed-certificate-verify/57614113#57614113
---
+++ automation-tasks/send_email.py
@@ -0,0 +1,17 @@
+import smtplib, ssl
+
+def send_email(message):
+    port = 465  # For SSL
+    smtp_server = "smtp.gmail.com"
+    sender_email = ""
+    receiver_email = ""
+    password = ""
+    
+    context = ssl.create_default_context()
+    with smtplib.SMTP_SSL(smtp_server, port, context=context) as server:
+        try:
+            server.login(sender_email, password)
+            res = server.sendmail(sender_email, receiver_email, message)
+            print('email sent!')
+        except:
+            print("could not login or send the email.")
