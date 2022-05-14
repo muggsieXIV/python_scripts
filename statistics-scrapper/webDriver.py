d84d0c106ba82a50dd9782b16ce1315e174da14a
muggsieXIV <benwillwalk@gmail.com>
2022-05-13 21:53:00 -0600

    working webdriver

--- statistics-scrapper/webDriver.py
+++ statistics-scrapper/webDriver.py
@@ -1,7 +1,8 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+from webdriver_manager.chrome import ChromeDriverManager
 
-driver = webdriver.Chrome(executable_path='C:/path/to/chromedriver.exe')
+driver = webdriver.Chrome(ChromeDriverManager().install())
 driver.get("http://www.python.org")
 assert "Python" in driver.title
 elem = driver.find_element_by_name("q")
